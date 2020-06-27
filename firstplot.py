4e10ee8b32e2e4e18c7131a7b0634746609c3bc3
Ormorod <Adam.Ormondroyd@gmail.com>
2020-06-27 17:59:31 +0100

    Speeded up lookup
    Lookup speed increased (no longer has to look repeatedly) but no plot as of yet
    Also realised that first letter of change should always be the same

--- firstplot.py
+++ firstplot.py
@@ -3,28 +3,32 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from lookup import lookup_frequency, AGES, CHANGES, age_lane_map
+from lookup import lookup, AGES, CHANGES, LANES, age_lane_map
 
 fig, ax = plt.subplots()
 
-id = "als5"
+person = "als5"
 chromosome = "chr1"
 position = 36931702
-plot_title = id + chromosome + str(position)
+plot_title = person + chromosome + str(position)
 
 mutation_frequencies = np.zeros(AGES.size)
 
-for change in CHANGES:
-    print(change)
-    for i in range(AGES.size):
-        mutation_frequencies[i] = lookup_frequency(
-            id, age_lane_map[AGES[i]], chromosome, position, change
-        )
+# for change in CHANGES:
+#     print(change)
+#     for i in range(AGES.size):
+#         mutation_frequencies[i] = lookup_frequency(
+#             id, age_lane_map[AGES[i]], chromosome, position, change
+#         )
 
-    ax.plot(AGES, mutation_frequencies, label=change, marker="+", linestyle="None")
-ax.set(title=plot_title, xlabel="age", ylabel="frequency of mutation")
-ax.set_xticks(AGES)
-ax.legend()
+#     ax.plot(AGES, mutation_frequencies, label=change, marker="+", linestyle="None")
+# ax.set(title=plot_title, xlabel="age", ylabel="frequency of mutation")
+# ax.set_xticks(AGES)
+# ax.legend()
 
-plt.savefig("plots\\" + plot_title + ".png")
-plt.show()
+df = lookup(person, LANES, chromosome, position, CHANGES)
+print("something")
+print(df)
+
+# plt.savefig("plots\\" + plot_title + ".png")
+# plt.show()
--- lookup.py
+++ lookup.py
@@ -2,26 +2,12 @@ import numpy as np
 import pandas as pd
 
 AGES = np.array([0, 7, 17, 24])
+LANES = np.array(["lane1", "lane2", "lane3", "lane4"])
 age_lane_map = {0: "lane1", 7: "lane2", 17: "lane3", 24: "lane4"}
+lane_age_map = {"lane1": 0, "lane2": 7, "lane3": 17, "lane4": 24}
 
 CHANGES = np.array(
-    [
-        "TA",
-        "TC",
-        "TG",
-        "AC",
-        "AG",
-        "AT",
-        "CA",
-        "CG",
-        "CT",
-        "AC",
-        "AG",
-        "AT",
-        "AC",
-        "AG",
-        "AT",
-    ]
+    ["TA", "TC", "TG", "AC", "AG", "AT", "CA", "CG", "CT", "AC", "AG", "AT",]
 )
 
 id_df = pd.read_csv(
@@ -33,37 +19,47 @@ id_df = pd.read_csv(
 )
 
 
-def lookup_frequency(id, lane, chromosome, position, change):
+def lookup(person, lanes, chromosome, position, changes):
     """
-    Looks up a given person, age (given by lane) and transition (e.g. AC)
+    Looks up a given person, ages (given by lanes) and transitions (e.g. AC).
     "lane1" = age0, "lane2" = age7, "lane3" = age17, "lane4" = age24
     """
-    sample_id = id_df.at[id, lane][:14]
+    sample_ids = np.zeros(
+        lanes.size, dtype="U14"
+    )  # string length 14, keep an eye on this
 
+    for i in range(lanes.size):
+        print(id_df.at[person, lanes[i]][:14])
+        sample_ids[i] = id_df.at[person, lanes[i]][:14]
+
+    print(sample_ids)
     chunksize = 10 ** 6  # number of rows per chunk
+    column_names = [
+        "chromosome",
+        "position",
+        "change",
+        "frequency",
+        "num consensus molecules",
+        "sample ID",
+    ]
+    # Empty dataframe
+    df = pd.DataFrame(columns=column_names)
 
     for chunk in pd.read_csv(
         "data_files\\full_data.txt",
         chunksize=chunksize,
         header=None,
-        names=[
-            "chromosome",
-            "position",
-            "change",
-            "frequency",
-            "num consensus molecules",
-            "sample ID",
-        ],
+        names=column_names,
         sep="\t",
     ):
 
-        a = chunk.loc[
-            (chunk["sample ID"] == sample_id)
-            & (chunk["change"] == change)
+        df_chunk = chunk.loc[
+            (chunk["sample ID"].isin(sample_ids))
+            & (chunk["change"].isin(changes))
             & (chunk["position"] == position)
             & (chunk["chromosome"] == chromosome)
-        ]["frequency"].values.tolist()
+        ]
+        print(df_chunk)
+        df = df.append(df_chunk, ignore_index=True)
 
-        if a:
-            return a[0]
-    return
+    return df
