cb4b5fb1eeddd4fb9c3ab3b0d838111743a97fef
Ormorod <Adam.Ormondroyd@gmail.com>
2020-08-03 20:23:37 +0100

    plot_all_mean_var now shows what chromosome each point is from

--- poisson_test.py
+++ poisson_test.py
@@ -1,10 +1,11 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from lookup import gene_seqs_map, seq_df, seq_data_df
 from constants import SUBS
 
 
-def mean_var(sequence_number, sub):
+def mean_var(sequence_number, sub, chromosome):
     """
     Returns  of mean and var for a given sequence and sub.
     """
@@ -17,26 +18,43 @@ def mean_var(sequence_number, sub):
 
 
 def plot_all_mean_var():
-    means = np.zeros(len(seq_df.index))
-    variances = np.zeros(len(seq_df.index))
-
     for sub in SUBS:
         print(sub)
         fig, ax = plt.subplots(3, figsize=(8, 8))
-        for i in seq_df.index:
-            print(i)
-            means[i], variances[i] = mean_var(i, sub)
-        ax[0].plot(seq_df.index, means, label="means", marker="+", linestyle="None")
+
+        for chromosome in pd.unique(seq_df["chromosome"]):
+            chr_seq_df = seq_df.loc[seq_df["chromosome"] == chromosome]
+            means = np.zeros(len(chr_seq_df.index))
+            variances = np.zeros(len(chr_seq_df.index))
+            for i, index in enumerate(chr_seq_df.index):
+                print(i)
+                means[i], variances[i] = mean_var(index, sub, chromosome)
+
+            ax[0].plot(
+                chr_seq_df.index,
+                means,
+                label="means",
+                marker="${}$".format(chromosome),
+                linestyle="None",
+            )
+
+            ax[1].plot(
+                chr_seq_df.index,
+                variances,
+                label="variances",
+                marker="${}$".format(chromosome),
+                linestyle="None",
+            )
+
+            ax[2].plot(
+                chr_seq_df.index,
+                variances / means,
+                label="means",
+                marker="${}$".format(chromosome),
+                linestyle="None",
+            )
         ax[0].set(title="means", xlabel="sequence", ylabel="mean", yscale="log")
-
-        ax[1].plot(
-            seq_df.index, variances, label="variances", marker="+", linestyle="None"
-        )
         ax[1].set(title="variances", xlabel="sequence", ylabel="variance", yscale="log")
-
-        ax[2].plot(
-            seq_df.index, variances / means, label="means", marker="+", linestyle="None"
-        )
         ax[2].set(title="ratios", xlabel="sequence", ylabel="ratios", yscale="log")
         fig.tight_layout()
         # fig.savefig("plots\\means_and_variances\\{}_mean_var.png".format(sub))
